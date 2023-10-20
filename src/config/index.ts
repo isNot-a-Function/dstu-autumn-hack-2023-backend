cd5e18b072b60c8175f72982cb1fe214bc3b9645
Nikolay Kovalev <nick.max.kovaly@gmail.com>
2023-10-20 22:30:15 +0300

    fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 export const SERVER_TYPE = process.env.SERVER_TYPE;
 export const HASH_COIN = Number(process.env.HASH_COIN) || 7;
 export const SERVICE_NAME = process.env.SERVICE_NAME || 'dstu-autumn-hack-2023';
-export const SERVER_HOST = process.env.SERVER_HOST || '127.0.0.1';
+export const SERVER_HOST = process.env.SERVER_HOST || '0.0.0.0';
 export const SERVER_PORT = process.env.SERVER_PORT || '5100';
 export const COOKIE_SECRET = process.env.COOKIE_SECRET || 'COOKIE_OC_SECRET';
 export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || 'JWT_REFRESH';
