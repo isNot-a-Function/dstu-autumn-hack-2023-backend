714129e093ee428dac1508ccd7d1025255618634
RykivSale <coolmrant@yandex.com>
2023-10-21 12:32:31 +0300

    feature: add guestion-answer system

--- src/algoritms/api.py
+++ src/algoritms/api.py
@@ -6,22 +6,39 @@ import json
 
 from typing import List, Optional
 from pydantic import BaseModel
-from calculate_answer import get_answer_by_ai
+from calculate_answer import get_answer_by_ai_by_definition,get_answer_by_ai_message_by_question
 app = FastAPI()
 
-class Message(BaseModel):
+class MessageDefinitionType(BaseModel):
     content: str
 
+class MessageQuestionType(BaseModel):
+    question: str
+    answer: str
 
 
-@app.post("/get_answer_by_ai")
-async def get_freeze_recomendation(data: Message):
+@app.post("/get_answer_by_ai_by_definition")
+async def get_by_definition(data: MessageDefinitionType):
 
     # Создаем экземпляр RootModel из данных
-    root_message = Message.model_validate(data)
+    root_message = MessageDefinitionType.model_validate(data)
 
-    result = get_answer_by_ai(root_message.content)
+    result = get_answer_by_ai_by_definition(root_message.content)
     data = {"ai_answer": result}
     json_data = json.dumps(data)
 
     return(json_data)
+
+
+@app.post("/get_answer_by_ai_message_by_question")
+async def get_by_question(data: MessageQuestionType):
+
+    # Создаем экземпляр RootModel из данных
+    root_message = MessageQuestionType.model_validate(data)
+
+    result = get_answer_by_ai_message_by_question(root_message.question,root_message.answer)
+    data = {"ai_answer": result}
+    json_data = json.dumps(data)
+
+    return(json_data)
+
--- src/algoritms/calculate_answer.py
+++ src/algoritms/calculate_answer.py
@@ -7,9 +7,36 @@ def translate_to_english(text):
     translated = translator.translate(text, src='ru', dest='en')
     return translated.text
 
+def get_answer_by_ai_message_by_question(question,answer):
+  g4f.logging = False # enable logging
+  g4f.check_version = False # Disable automatic version checking
+  print(g4f.version) # check version
+  print(g4f.Provider.Ails.params)  # supported args
 
+  question_eng = translate_to_english(question)
+  answer_eng = translate_to_english(answer)
+  # Automatic selection of provider
+  request = f"""You have to work as a validation of the answers. I'm giving you the question and the user's answer.
+  Analyzing the context, it is necessary to answer correctly or not the user answered.
+  Please answer only with the words 'yes' if the definition is true or 'no' if it is false.
+  Question: "{question_eng}";
+  User Answer: "{answer_eng}"
+  """
+  # streamed completion
+  response = g4f.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[{"role": "user", "content": request}],
+      stream=True,
+  )
 
-def get_answer_by_ai(message):
+  for message in response:
+      result_text+=message
+  result = False
+  if('yes' in result_text.lower()):
+    result = True
+  return result
+
+def get_answer_by_ai_by_definition(message):
   g4f.logging = False # enable logging
   g4f.check_version = False # Disable automatic version checking
   print(g4f.version) # check version
