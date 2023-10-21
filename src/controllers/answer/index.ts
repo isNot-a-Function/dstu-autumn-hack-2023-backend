ace367094b0e07ca4547aeef1a9487a2366891c5
Nikolay Kovalev <nick.max.kovaly@gmail.com>
2023-10-22 01:01:45 +0300

    fix

--- src/controllers/answer/index.ts
+++ src/controllers/answer/index.ts
@@ -7,26 +7,69 @@ import { SuccessReply } from '../../reply/success.reply';
 import { ErrorReply } from '../../reply/error.reply';
 
 import { logger } from '../../log';
+import { verifyAccessToken } from '../../integrations/jwt';
 import { ISendAnswer } from './interface';
 import { SendAnswerSchema } from './validator';
 
-export const GetDirectionsBySpecializationController = async (
+export const SendAnswerController = async (
   req: FastifyRequest<{ Body: ISendAnswer }>,
   reply: FastifyReply,
 ) => {
   try {
+    if (!req.headers.authorization) {
+      reply
+        .status(ErrorReply.TokenIsNotExistErrorStatus);
+
+      return;
+    }
+
+    const user = verifyAccessToken(req.headers.authorization);
+
+    if (typeof user === 'string') {
+      reply
+        .status(ErrorReply.TokenIsInvalidErrorStatus);
+
+      return;
+    }
+
     const data = SendAnswerSchema.parse(req.query);
+
+    const test = await prisma.test.findUnique({
+      where: {
+        id: data.testId,
+      },
+      select: {
+        response: true
+      }
+    })
+
+    const response = await prisma.response.findFirst({
+      where: {
+        userId: user.userId,
+        direction:
+      }
+    })
+
+    const answer = await prisma.answer.create({
+      data: {
+        test: {
+          connect: {
+            id: data.testId,
+          },
+        },
+      },
+    });
 
     // const directions = await prisma.answer.create({
     //   data:
 
     // });
 
-    // reply
-    //   .status(SuccessReply.DataSendSuccessStatus)
-    //   .send({
-    //     directions,
-    //   });
+    reply
+      .status(SuccessReply.DataSendSuccessStatus)
+      .send({
+        directions,
+      });
   } catch (error) {
     if (error instanceof ZodError) {
       logger.error(error.message);
